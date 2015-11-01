0389106d7b5600511800e317041954fd8556589a
Roger Padilla <rogerjose81@gmail.com>
2015-11-01 09:19:22 -0500

    Simplified karma.conf.js and unified with the one from the ng2 repo.

--- karma.conf.js
+++ karma.conf.js
@@ -4,14 +4,9 @@
 module.exports = function(config) {
   config.set({
 
-    // base path that will be used to resolve all patterns (eg. files, exclude)
-    basePath: './',
-
-
     // frameworks to use
     // available frameworks: https://npmjs.org/browse/keyword/karma-adapter
     frameworks: ['jasmine'],
-
 
     // list of files / patterns to load in the browser
     files: [
@@ -28,45 +23,22 @@ module.exports = function(config) {
       { pattern: 'node_modules/systemjs/dist/system-polyfills.js', included: false, watched: false }, // PhantomJS2 (and possibly others) might require it
       { pattern: 'node_modules/angular2/**/*.js', included: false, watched: false },
             
-      { pattern: 'test/**/*.js', included: false, watched: true },      
+      { pattern: 'test/**/*.js', included: false, watched: false },      
       'tools/build/file2modulename.js',
       'test-main.js'
     ],
 
-
     // list of files to exclude
     exclude: [
     ],
-
-
-    // preprocess matching files before serving them to the browser
-    // available preprocessors: https://npmjs.org/browse/keyword/karma-preprocessor
-    preprocessors: {
-    },
-
 
     // test results reporter to use
     // possible values: 'dots', 'progress'
     // available reporters: https://npmjs.org/browse/keyword/karma-reporter
     reporters: ['mocha'],
 
-
     // web server port
     port: 9876,
-
-
-    // enable / disable colors in the output (reporters and logs)
-    colors: true,
-
-
-    // level of logging
-    // possible values: config.LOG_DISABLE || config.LOG_ERROR || config.LOG_WARN || config.LOG_INFO || config.LOG_DEBUG
-    logLevel: config.LOG_INFO,
-
-
-    // enable / disable watching file and executing tests whenever any file changes
-    autoWatch: true,
-
 
     // start these browsers
     // available browser launchers: https://npmjs.org/browse/keyword/karma-launcher
@@ -75,18 +47,12 @@ module.exports = function(config) {
       'PhantomJS2'
     ],
 
-
     customLaunchers: {
       Chrome_travis_ci: {
         base: 'Chrome',
         flags: ['--no-sandbox']
       }
-    },
-
-
-    // Continuous Integration mode
-    // if true, Karma captures browsers, runs the tests and exits
-    singleRun: false
+    }
   });
 
   if (process.env.TRAVIS) {
